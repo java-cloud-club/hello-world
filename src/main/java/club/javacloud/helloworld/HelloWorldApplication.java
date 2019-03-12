c423bc693c9dfb5d6afe977c9e2abea8006ced5a
metamaker <dmytro.yurchenko@sio.com>
2019-03-12 17:43:13 +0100

    Add JProfiler and introduce memory leak

--- src/main/java/club/javacloud/helloworld/HelloWorldApplication.java
+++ src/main/java/club/javacloud/helloworld/HelloWorldApplication.java
@@ -3,8 +3,11 @@ package club.javacloud.helloworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashSet;
+
 @SpringBootApplication
 public class HelloWorldApplication {
+	public static HashSet<String> iAmMemoryLeak = new HashSet<>();
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
--- src/main/java/club/javacloud/helloworld/HelloWorldController.java
+++ src/main/java/club/javacloud/helloworld/HelloWorldController.java
@@ -3,11 +3,17 @@ package club.javacloud.helloworld;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static club.javacloud.helloworld.HelloWorldApplication.iAmMemoryLeak;
+
 @RestController
 public class HelloWorldController {
     @GetMapping(path = "/")
     public String sayHello()
     {
+        for (int i = 1; i < 10000; ++i) {
+            iAmMemoryLeak.add("life is nice");
+        }
+
         return "Hello World!";
     }
 }
